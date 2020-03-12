3182d77e062ccdf9f184722c2c90a2c1dd89eb7a
felixwinangun <felixwinangun@ymail.com>
2020-03-12 23:30:52 +0700

    add google auth

--- controllers/UserC.js
+++ controllers/UserC.js
@@ -3,6 +3,8 @@ const jwt = require('../helpers/jwt');
 const CustomError = require('../helpers/customError');
 const bcrypt = require('../helpers/bcrypt');
 const invalid = "invalid email / password!";
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 class Controller {
     static login(req, res, next) {
@@ -36,8 +38,51 @@ class Controller {
     }
 
     static loginGoogle(req, res, next) {
-        let err = new CustomError(500, "belum dibuat boi!");
-        next(err);
+        let token = req.headers.token;
+        let isNew = false;
+        let email = "";
+        client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+            .then((result) => {
+                const payload = result.getPayload();
+                email = payload.email;
+                return User.findOne({
+                    where: {
+                        email
+                    }
+                })
+            })
+            .then(result => {
+                if(result) {
+                    // ketemu kirim token
+                    isNew = false;
+                    return result;
+                } else {
+                    // ga ketemu register
+                    isNew = true;
+                    return User.create({
+                        email,
+                        password: email + "g"
+                    })
+                }
+            })
+            .then(result => {
+                let payload = {
+                    id: result.id
+                }
+                payload = jwt.createToken(payload);
+                let status = 200;
+                if(isNew){
+                    status = 201;
+                }
+                res.status(status).json({ token: payload })
+            })
+            .catch((err) => {
+                err = new CustomError(500, "error in google!");
+                next(err);
+            });
     }
 
     static register(req, res, next) {
