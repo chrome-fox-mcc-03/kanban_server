3ecc35a5e2a6b184cab2f94d40742fe95f2f2b6c
bhaktitud <myforework@live.com>
2020-03-13 19:31:34 +0700

    redeploy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(routes)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`listening to : ${PORT}`);
