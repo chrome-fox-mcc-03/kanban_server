01a667da00c429f918f9e592600c5872da344f12
abdulbasithk <abdulbasithkamaludin@gmail.com>
2020-03-13 21:37:43 +0700

    Final Commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@ const { User } = require('../models')
 const { getToken } = require('../helpers/jwt')
 const getName = require('../helpers/getName')
 const { comparePassword } = require('../helpers/bcrypt')
-
+const {OAuth2Client} = require('google-auth-library');
 class UserController {
     static register(req, res, next) {
         let name = req.body.name 
@@ -56,6 +56,48 @@ class UserController {
                 }
             })
             .catch(next)
+    }
+    static googleLogin(req, res, next) {
+        const client = new OAuth2Client(process.env.CLIENT_ID);
+        let emailGoogle;
+        let name;
+        client.verifyIdToken({
+            idToken: req.body.token,
+            audience: process.env.CLIENT_ID
+        })
+            .then(response => {
+                emailGoogle = response.payload.email
+                name = response.payload.name
+                return User.findOne({
+                    where: {
+                        email:emailGoogle
+                    }
+                })
+            })
+            .then(user => {
+                if(user) {                        
+                    return user
+                }
+                else {
+                    return User.create({
+                        email: emailGoogle,
+                        password: 'password',
+                        name
+                    })
+                }
+            })
+            .then(response => {
+                let payload = {
+                    id: response.id,
+                    email: response.email,
+                    name: response.name
+                }
+                let token = getToken(payload)
+                res.status(201).json({ token, name })
+            })
+            .catch(err => {
+                next(err)
+            })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/UserController')
 const authentication = require('../middlewares/authentification')
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/googleLogin', UserController.googleLogin)
 
 router.use(authentication)
 
