0dbf085d3ff845d718d0ce21d16c7e3d731b786a
tamarazu <tamarazulaika.u@gmail.com>
2020-03-13 22:24:18 +0700

    add Google Login

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,11 @@
 const { User } = require('../models')
 const { checkingPassword } = require('../helpers/bcrypt')
 const { generateToken }= require('../helpers/jwt')
+const PRIVATKEY = process.env.PRIVATKEY
+const CLIENTID = process.env.CLIENTID
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(PRIVATKEY)
+
 
 class UserController {
     static register(req, res, next){
@@ -69,6 +74,46 @@ class UserController {
                         message: 'User Not Found!'
                     })
                 }
+            })
+            .catch(next)
+    }
+
+    static googleSignIn(req, res, next) {
+        console.log('KE HIT NEEEEHHH++++++++++++++++++++++++')
+        let email = ""
+        let newUser = {}
+        client 
+            .verifyIdToken({
+                idToken : req.headers.access_token,
+                audience : CLIENTID
+            })
+            .then( googleUser => {
+                newUser = {
+                    email: googleUser.payload.email,
+                    password: process.env.GOOGLE_PASSWORD
+                }
+                email = googleUser.payload.email
+
+                return User.findOne({
+                     where : {
+                         email: email
+                      }
+                })
+            })
+            .then( data => { 
+                if(!data) {
+                    return User.create(newUser)
+                } else return data
+            })
+            .then(data => {
+                let payload = {
+                    id: data.id,
+                    email: email
+                };
+                let access_token = generateToken(payload);
+                res.status(200).json({
+                    access_token : access_token
+                });
             })
             .catch(next)
     }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const UserController = require('../controllers/user')
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.post('/find', UserController.findOne)
+router.post('/googleSignIn', UserController.googleSignIn)
 
 module.exports = router
