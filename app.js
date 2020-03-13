e78943bfa5e0726eb64d94466e80f35bcb097334
prmzk <adam.primarizki@gmail.com>
2020-03-13 20:55:20 +0700

    fix socket deploying

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const cors = require('cors')
 const router = require('./routes/index.js')
 const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT
-const server = app.listen(3001)
-const io = require("socket.io")(server)
+var http = require('http').createServer(app)
+const io = require("socket.io")(http)
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
@@ -25,4 +25,4 @@ io.on('connection', function(socket){
     })
 })
 
-app.listen(PORT, () => console.log(`You are now listening to port`, PORT))
+http.listen(PORT, () => console.log(`You are now listening to port`, PORT))
