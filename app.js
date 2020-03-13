08fda0e8b2339ca5f161b42815d99a1630973488
Yoko <baghram0001@gmail.com>
2020-03-14 00:16:54 +0700

    deployment fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
 app.use(router)
-app.use(errhandler)
+// app.use(errhandler)
 
 app.listen(process.env.PORT, function() {
    console.log(`listening to port ${process.env.PORT}`)
--- middleware/errhandler.js
+++ middleware/errhandler.js
@@ -1,27 +1,27 @@
-module.exports = function(err, req, res, next) {
-    if(err.name) {
-        switch (err.name) {
+// module.exports = function(err, req, res, next) {
+//     if(err.name) {
+//         switch (err.name) {
 
-            default:
-                let msg = {
-                    msg: 'Bad Request'
-                }
-                res.status(400).json(msg)
-                break;
-        }
-    }
-    else {
-        switch (err.msg) {
-            case "This Email Is Not Registered":
-                res.status(400).json(err)
-                break;
+//             default:
+//                 let msg = {
+//                     msg: 'Bad Request'
+//                 }
+//                 res.status(400).json(msg)
+//                 break;
+//         }
+//     }
+//     else {
+//         switch (err.msg) {
+//             case "This Email Is Not Registered":
+//                 res.status(400).json(err)
+//                 break;
         
-            default:
-                let msg = {
-                    msg: 'Bad Request'
-                }
-                res.status(400).json(msg)
-                break;
-        }
-    }
-}
+//             default:
+//                 let msg = {
+//                     msg: 'Bad Request'
+//                 }
+//                 res.status(400).json(msg)
+//                 break;
+//         }
+//     }
+// }
