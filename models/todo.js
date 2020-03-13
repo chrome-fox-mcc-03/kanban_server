c15adf416dc667a54ddd2a73aa80d54ff530f02d
thomasmartuaps <thomasmartuaps@gmail.com>
2020-03-13 18:01:38 +0700

    Initial Commit

---
+++ models/todo.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Model = sequelize.Sequelize.Model
+  class Todo extends Model {}
+  Todo.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 100]
+      },
+    },
+    description: DataTypes.STRING,
+    status: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER
+  }, {
+    hooks: {
+      beforeCreate(todo, options) {
+        if(!todo.description) todo.description = 'No description added.'
+      }
+    }
+  });
+  Todo.associate = function(models) {
+    // associations can be defined here
+    Todo.belongsTo(models.User)
+  };
+  return Todo;
+};
