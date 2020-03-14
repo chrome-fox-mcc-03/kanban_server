24339633ac80d37b2b73d48bbcc3cf0b07375d5f
adam jay <adamjay041@gmail.com>
2020-03-14 13:22:25 +0700

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const errorHandler = require('./middleware/errorHandler')
 const router =require('./routes')
 
