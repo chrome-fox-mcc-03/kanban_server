3b6336b826d8939a23d0e19090e846744d378843
abdulbasithk <abdulbasithkamaludin@gmail.com>
2020-03-14 12:33:09 +0700

    Abdul Basith K

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 const app = express()
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
+var http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
@@ -14,5 +17,10 @@ app.use(express.json())
 app.use(router)
 
 app.use(errorHandler)
-
-app.listen(process.env.PORT, () => console.log('listening to port: ' + process.env.PORT))
+io.on('connection', function(socket) {
+    console.log('User Connected')
+    socket.on('new_task', function() {
+        io.emit('new_task_arrived')
+    })
+})
+http.listen(process.env.PORT, () => console.log('listening to port: ' + process.env.PORT))
