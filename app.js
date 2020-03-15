cd5c65628bb0418b0ad1fe1c8ffe643854b340a3
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-15 22:36:19 +0700

    deploy

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT =  process.env.PORT || 3000
 const router = require('./routes/index')
 const cors = require('cors')
+const errorHandler = require('./middlewares/errorHandler')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(router)
+app.use(erro)
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
