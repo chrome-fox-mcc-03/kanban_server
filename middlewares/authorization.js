75624c5a860c9d266af257e2f6680079baa36643
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-16 17:19:39 +0700

    add authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,22 +1,32 @@
-// const { Collaboration } = require('../models')
+const { Project, Collaboration } = require('../models')
+function ProjectAuthorization(req, res, next) {
+    Project.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(project => {
+        if (project) {
+            const ProjectId = project.id
+            return Collaboration.findOne({ where: { ProjectId } })
+                .then(collaboration => {
+                    if (collaboration) {
+                        if (collaboration.UserId == req.currentUserId) {
+                            next()
+                        } else {
+                            next({ name: 'Unauthorized' })
+                        }
+                    } else {
+                        next({ name: '404NotFound' })
+                    }
+                })
+        } else {
+            next({ name: '404NotFound' })
+        }
+    }).catch(err => {
+        next(err)
+    })
+}
 
-// module.exports = function (req, res, next) {
-//     Collaboration.findOne({
-//         where: {
-//             id: req.params.id
-//         }
-//     }).then(result => {
-//         if (result) {
-//             if (result.UserId == req.currentUserId) {
-//                 next()
-//             } else {
-//                 next({ name: 'Unauthorized' })
-//             }
-//         } else {
-//             next({ name: '404NotFound' })
-//         }
-//     }).catch(err => {
-//         next(err)
-//     })
-// }
-
+module.exports = {
+    ProjectAuthorization
+}
--- routes/project_routes.js
+++ routes/project_routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const ProjectController = require('../controllers/ProjectController')
 const authentication = require('../middlewares/authentication')
+const { ProjectAuthorization } = require('../middlewares/authorization')
+
 
 router.use(authentication)
 router.post('/', ProjectController.create)
 router.get('/', ProjectController.findAll)
-router.get('/:id', ProjectController.findOne)
-router.delete('/:id', ProjectController.delete)
+router.get('/:id', ProjectAuthorization, ProjectController.findOne)
+router.delete('/:id', ProjectAuthorization, ProjectController.delete)
 
 
 module.exports = router
